7310fdec87f4eb5cb80fa86d1bd54c13dc711b01
神馬隆博 <takahiro412.c@gmail.com>
2020-05-04 08:30:13 +0900

    メールで送信するURLリンクを、本番環境ではhttpsにするよう変更

--- resources/views/emails/templates/password_reset_request_mail.blade.php
+++ resources/views/emails/templates/password_reset_request_mail.blade.php
@@ -2,6 +2,10 @@
 パスワードをリセットするには、下記のリンクをクリックしてください。
 リンク有効期限は{{PASSWORD_RESET_EXPIRATION}}分間です。
 
+@if(App::environment('local'))
 {{ url('user/pw/reset/input', ['user_id' => $sendData['user_id'], 'token' => $sendData['token']]) }}
+@else
+{{ url('user/pw/reset/input', ['user_id' => $sendData['user_id'], 'token' => $sendData['token']], true) }}
+@endif
 
 上記の内容に心当たりがない場合はサイト管理者までご連絡ください。
